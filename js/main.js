7f092bbd04b6f4e501d617d08c5f646dc181ef68
jounicorn <jogithub@gmail.com>
2022-10-24 14:56:50 +0900

    no resp

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 var config = {
     type: Phaser.AUTO,
     scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'game',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
+/*    scale: {
         mode: Phaser.Scale.RESIZE,
         parent: 'game',
         width: 800,
@@ -14,6 +21,7 @@ var config = {
             height: 600
         }
     },
+*/
     physics: {
         default: 'arcade',
         arcade: {
