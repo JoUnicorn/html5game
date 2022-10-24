4d31a9a09958973e94b91cd56e8f7e8e375f37c5
jounicorn <jogithub@gmail.com>
2022-10-25 01:21:35 +0900

    dsfsd

--- js/main.js
+++ js/main.js
@@ -35,7 +35,8 @@ var config = {
         create: create,
         update: update,
         extend: {
-            createSpeechBubble: createSpeechBubble
+            createSpeechBubble: createSpeechBubble,
+            nextWord: nextWord
         }
     }
 };
