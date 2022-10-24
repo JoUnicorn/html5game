428044d3a9a05391580895a9c8a6722c456bbcad
jounicorn <jogithub@gmail.com>
2022-10-24 21:02:06 +0900

    test

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@ function create() {
     player.setCollideWorldBounds(true); // don't go out of the map
 
     // small fix to our player images, we resize the physics body object slightly
-    player.body.setSize(player.width, player.height-8);
+    player.body.setSize(player.width, player.height);
 
     // player will collide with the level tiles
     this.physics.add.collider(groundLayer, player);
