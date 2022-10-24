82568531411bafc94f1f9e6862b5c735ee61b33f
jounicorn <jogithub@gmail.com>
2022-10-24 14:13:03 +0900

    responsive

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var config = {
     type: Phaser.AUTO,
     scale: {
-        mode: Phaser.Scale.FIT,
+        mode: Phaser.Scale.RESIZE,
         parent: 'phaser-example',
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 800,
-        height: 600
+        width: 400,
+        height: 300
     },
     physics: {
         default: 'arcade',
