7a6ed983979429cf00377bfe40ce9c6442f36481
jounicorn <jogithub@gmail.com>
2022-10-25 00:38:48 +0900

    fff

--- js/main.js
+++ js/main.js
@@ -203,7 +203,7 @@ function create() {
     infoText.setScrollFactor(0);
 
     // modal
-    this.createSpeechBubble(20, 100, 320, 160, "jmbfdsjbfvjd");
+    this.createSpeechBubble(20, 100, 320, 160, "he sky above the port was the color of television, tuned to a dead channel.");
 
     // gr
     timedEvent = this.time.delayedCall(3000, startWalking_gr, [], this);
