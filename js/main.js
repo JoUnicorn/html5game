b6e41f4c1cc780bfc038ead08ed1a0edede1e47b
jounicorn <jogithub@gmail.com>
2022-10-25 15:36:36 +0900

    gg

--- js/main.js
+++ js/main.js
@@ -294,8 +294,8 @@ function create() {
     timedEvent = this.time.delayedCall(15500, startWalking_jo, [], this);
     timedEvent = this.time.delayedCall(19500, stopWalking_jo, [], this);
     timedEvent = this.time.delayedCall(23500, startWalking_jo, [], this);
-    timedEvent = this.time.delayedCall(33500, stopWalking_jo, [], this);
-    timedEvent = this.time.delayedCall(30500, generate, [3500,1700], this);
+    timedEvent = this.time.delayedCall(29500, stopWalking_jo, [], this);
+    timedEvent = this.time.delayedCall(33500, generate, [3500,1700], this);
 
     //timedEvent = this.time.delayedCall(10000, rescale_jo, [], this);
     //timedEvent = this.time.delayedCall(10500, startWalking_jo, [], this);
@@ -388,6 +388,7 @@ function rescale_jo()
 
 // this function will be called when the player touches a coin
 function eat_mus() {
+  fireball.visible=false;
   this.tweens.add({
       targets: mushroom,
       alpha: 0,
