d8a4adaedfc92e554237700c0353dfc7fff0cb2d
jounicorn <jogithub@gmail.com>
2022-10-25 19:35:46 +0900

    f

--- js/main.js
+++ js/main.js
@@ -86,7 +86,7 @@ function preload() {
 
 class Button {
     constructor(x, y, label, scene, callback) {
-        const button = scene.add.text(x, y, label)
+        const button = this.add.text(x, y, label)
             .setOrigin(0.5)
             .setPadding(10)
             .setStyle({ backgroundColor: '#111' })
