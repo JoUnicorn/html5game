aa0d6db1d830b7cd921f37a9fac4eb8bd65e37c4
jounicorn <jogithub@gmail.com>
2022-10-25 19:33:24 +0900

    ff

--- js/main.js
+++ js/main.js
@@ -99,7 +99,7 @@ class Button {
 
 function create() {
     this.scene.pause();
-    background = this.add.tileSprite(0, 0, 800, 600, 'bg');
+    //background = this.add.tileSprite(0, 0, 800, 600, 'bg');
 
     // And a label to illustrate which menu item was chosen. (This is not necessary)
     choiseLabel = this.add.text(20, 20, 'Click outside menu to continue', { font: '30px Arial', fill: '#fff' });
