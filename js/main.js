34195d2b479bfe9cb2f536918cd3aac0a0f725c8
jounicorn <jogithub@gmail.com>
2022-10-25 15:53:27 +0900

    fff

--- js/main.js
+++ js/main.js
@@ -301,7 +301,8 @@ function create() {
     timedEvent = this.time.delayedCall(15500, startWalking_jo, [], this);
     timedEvent = this.time.delayedCall(19500, stopWalking_jo, [], this);
     timedEvent = this.time.delayedCall(23500, startWalking_jo, [], this);
-    timedEvent = this.time.delayedCall(29000, stopWalking_jo, [], this);
+    timedEvent = this.time.delayedCall(28000, stopWalking_jo, [], this);
+    timedEvent = this.time.delayedCall(28100, move_camera, [], this);
     timedEvent = this.time.delayedCall(33500, generate, [3500,1700], this);
 
     //timedEvent = this.time.delayedCall(10000, rescale_jo, [], this);
@@ -334,6 +335,11 @@ function generate(x, y)
         ease: 'Sine.easeInOut'
     });
 
+}
+
+function move_camera()
+{
+    this.camera.x += 100;
 }
 
 function draw()
