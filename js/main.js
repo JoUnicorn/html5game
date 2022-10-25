90e3edbf25f23d80c75a6f141692d9673ea1e03a
jounicorn <jogithub@gmail.com>
2022-10-25 22:09:36 +0900

    dfgg

--- js/main.js
+++ js/main.js
@@ -425,7 +425,7 @@ function create() {
     timedEvent = this.time.delayedCall(67000, stopWalking_jo, [], this);
     timedEvent = this.time.delayedCall(69000, startWalking_jo, [], this);
     timedEvent = this.time.delayedCall(74000, startJumping_jo, [], this);
-    timedEvent = this.time.delayedCall(78000, startJumping_jo, [], this);
+    timedEvent = this.time.delayedCall(76000, startJumping_jo, [], this);
     timedEvent = this.time.delayedCall(80000, stopWalking_jo, [], this);
 
 
