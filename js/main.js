23749247f0e0dd87447b7794348fa8c0a649b6f4
jounicorn <jogithub@gmail.com>
2022-10-25 19:24:29 +0900

    ffff

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ function preload() {
     this.load.image('mushroom', 'assets/mushroom.png');
     this.load.image('fire', 'assets/muzzleflash3.png');
     this.load.image('boom', 'assets/muzzleflash2.png');
-    this.load.image('bg', 'assets/starfield.png');
+    this.load.image('bg', 'assets/bg2.png');
     // player animations
     this.load.atlas('player', 'assets/jo.png', 'assets/jo.json');
     this.load.atlas('girl', 'assets/girl.png', 'assets/girl.json');
