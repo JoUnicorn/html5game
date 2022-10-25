c0c515aaf766f3c44f4ad142543dd7eff1de51d1
jounicorn <jogithub@gmail.com>
2022-10-25 16:05:22 +0900

    ff

--- js/main.js
+++ js/main.js
@@ -309,6 +309,7 @@ function create() {
     console.log(zombie.x)
     console.log(zombie.y)
     timedEvent = this.time.delayedCall(33500, generate, [zombie.x,zombie.y], this);
+    console.log(this.camera)
 
     //timedEvent = this.time.delayedCall(10000, rescale_jo, [], this);
     //timedEvent = this.time.delayedCall(10500, startWalking_jo, [], this);
