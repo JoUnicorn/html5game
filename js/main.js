6fe770d3571a590747bd1c52b8424a4aa951d9fc
jounicorn <jogithub@gmail.com>
2022-10-25 19:23:49 +0900

    ddd

--- js/main.js
+++ js/main.js
@@ -364,7 +364,7 @@ function create() {
 
     //// demo scene
     this.scene.pause();
-    background = this.add.image(800, 600, 'bg');
+    background = this.add.image(0,0,800, 600, 'bg');
 
     // And a label to illustrate which menu item was chosen. (This is not necessary)
     choiseLabel = this.add.text(20, 20, 'Click outside menu to continue', { font: '30px Arial', fill: '#fff' });
